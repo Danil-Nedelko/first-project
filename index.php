11a77b0c783f1d52910b9a8b72a686911f65db29
Nedelko_Danil <Danya@mail.ru>
2025-02-14 15:30:37 +0700

    Добавить поле: Year of registration

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
+<h2>Text</h2>
+	<form action-"vendor/create.php" method="post">
+	<p>Owner</p>
+	<input type="test" name="Owner">
+	<p>Square</p>
+	<input type="number" name="Addres">
+	<button type="submit">Add</button>
  
